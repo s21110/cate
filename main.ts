c535fc1dc944ba8bb2dbad19f1991aa7ae5a10a2
s21110 <122764722+s21110@users.noreply.github.com>
2023-09-28 10:53:41 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . # . # .
+            # . . . #
+            # . . . #
+            # # # # #
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            . # . # .
+            # # # # #
+            # . . . #
+            # . . . #
+            . . . . .
+            `)
+    }
 })
